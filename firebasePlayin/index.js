a5b1e2d6cc77c2ce2b531b8bb09e386dab311b54
Milos <milos.codes@gmail.com>
2024-04-27 22:06:43 +0200

    firebase play!

--- firebasePlayin/index.js
+++ firebasePlayin/index.js
@@ -2,55 +2,140 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+
 const appSettings = {
     databaseURL: "https://fir-play-82d51-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-const app = initializeApp(appSettings)
+
+// create constants for app, database, and ref
+const app = appSettings()
 const database = getDatabase(app)
+const listInDB = ref(database, "someList")
 
 
-const randomListInDB = ref(database, "randomList")
 
-
+//create constants for input field, addbutton and addevent listener on button.
 const inputFieldEl = document.getElementById("input-field")
-const addButton = document.getElementById("add-button")
-const randomListEl = document.getElementById("random-list")
+const addButtonEl = document.getElementById("add-button")
+const listEl = document.getElementById("some-list")
 
-
-addButton.addEventListener("click", function(){
+addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    push(randomListInDB, inputValue)
+    push(listInDB, inputValue)
     console.log(inputValue)
     clearInputField()
-    
 })
 
-onValue(randomListInDB, function(snapshot) {
-    let itemsArray = Object.entries(snapshot.val()) 
-
-    clearRandomList()
-    for (let i=0; i<itemsArray.length;i++) {
+//call function onValue, with snapshot, use object inside to be enteries (create an array), so i can change that object from keys to values. 
+onValue(listInDB, function(snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+    clearListField()
+    for (let i=0; i < itemsArray.length; i++) {
         let currentItem = itemsArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        
+        let itemsIDs = currentItem[0]
+        let itemsValues = currentItem[1]
         appendItem(currentItem)
     }
-})
-function appendItem(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    // randomListEl.innerHTML += `<li>${item}</li>`
-    let newEl = document.createElement("li")
     
-    newEl.textContent = itemValue
-    randomListEl.append(newEl)
+})
+
+//make function appendItem() that takes value of item and appends. call it inside of function onValue in loop.
+function appendItem(item) {
+    let itemsID = item[0]
+    let itemsValue = item[1]
+    
+    let newEl = document.createElement("li")
+    newEl.textContent = itemsValue
+    listEl.append(newEl)
 }
 
+// make function clearInputField() that clears input field and call it in. use textContent to append new item
 function clearInputField() {
-    inputFieldEl.value = ""
+    inputFieldEl.textContent = ""
 }
-function clearRandomList() {
-    randomListEl.innerHTML = ""
+
+function clearListField() {
+    listEl.innerHTML = ""
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = initializeApp(appSettings)
+// const database = getDatabase(app)
+
+
+// const randomListInDB = ref(database, "randomList")
+
+
+// const inputFieldEl = document.getElementById("input-field")
+// const addButton = document.getElementById("add-button")
+// const randomListEl = document.getElementById("random-list")
+
+
+// addButton.addEventListener("click", function(){
+//     let inputValue = inputFieldEl.value
+//     push(randomListInDB, inputValue)
+//     console.log(inputValue)
+//     clearInputField()
+    
+// })
+
+// onValue(randomListInDB, function(snapshot) {
+//     let itemsArray = Object.entries(snapshot.val()) 
+
+//     clearRandomList()
+//     for (let i=0; i<itemsArray.length;i++) {
+//         let currentItem = itemsArray[i]
+//         let currentItemID = currentItem[0]
+//         let currentItemValue = currentItem[1]
+        
+//         appendItem(currentItem)
+//     }
+// })
+// function appendItem(item) {
+//     let itemID = item[0]
+//     let itemValue = item[1]
+//     // randomListEl.innerHTML += `<li>${item}</li>`
+//     let newEl = document.createElement("li")
+    
+//     newEl.textContent = itemValue
+//     randomListEl.append(newEl)
+// }
+
+// function clearInputField() {
+//     inputFieldEl.value = ""
+// }
+// function clearRandomList() {
+//     randomListEl.innerHTML = ""
+// }
